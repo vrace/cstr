429e699e5f98d0eaf5f13a23dca4b18de6cb91bc
Liu Yang <vrace_studios@hotmail.com>
2016-06-03 09:28:03 +0800

    Initial Commit

---
+++ cstr/cstr.h
@@ -0,0 +1,19 @@
+//
+//  cstr.h
+//  cstr
+//
+//  Created by Liu Yang on 6/3/16.
+//  Copyright © 2016 Liu Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cstr.
+FOUNDATION_EXPORT double cstrVersionNumber;
+
+//! Project version string for cstr.
+FOUNDATION_EXPORT const unsigned char cstrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cstr/PublicHeader.h>
+
+
